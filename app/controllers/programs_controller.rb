aab140280de5cfc9dea6a7a53695d14fc0901055
Dawn Mitchell <dmitchell@brandeis.edu>
2021-03-29 12:00:16 -0400

    Add ability to add new program

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,22 @@
 class ProgramsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    @program = Program.new(program_params)
+    if @program.save
+      flash[:success] = "Your program has been added!"
+      redirect_to add_review_path
+    else
+      render 'new'
+    end
+  end
+
+
+private
+
+  def program_params
+    params.permit(:company_id, :name, :field, :work_auth, :class_standing)
+  end
 end
