a314aedcfe89ec7e43eda325b0c8707a28aada52
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-07 16:45:17 -0400

    Modify error messages to work for all models

--- app/controllers/user_confids_controller.rb
+++ app/controllers/user_confids_controller.rb
@@ -13,7 +13,7 @@ class UserConfidsController < ApplicationController
         @user = UserConfid.new(user_params)
         if @user.save
           @user_acc = User.new(name: user_params[:name], major: nil, class_year: nil, bio: nil, title: nil, 
-            userconf_id: UserConfid.where(:email => user_params[:email]).first.id)
+            user_confid_id: UserConfid.where(:email => user_params[:email]).first.id)
           if @user_acc.save
             flash[:success] = "Welcome to the app!"
             redirect_to @user
