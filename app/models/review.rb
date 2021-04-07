0672e2655a619a652fefd2474afceabf877dbfcb
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-07 16:45:17 -0400

    Modify error messages to work for all models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :program
   belongs_to :user
+
+  validates :comment, presence: true
+  validates :collaboration, presence: true
+  validates :independence, presence: true
+  validates :multitasking, presence: true
+  validates :compensation, presence: true
 end
