1f6a3f4abb26e9b752077d6fcdb370c3859aa240
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-07 16:58:18 -0400

    Update program form and include validations

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,11 @@
 class Program < ApplicationRecord
   has_many :reviews
   belongs_to :company
+
+  validates :name, presence: true
+  validates :field, presence: true
+  validates :paid, presence: true
+  validates :work_auth, presence: true
+  validates :class_standing, presence: true
+  validates :season, presence: true
 end
