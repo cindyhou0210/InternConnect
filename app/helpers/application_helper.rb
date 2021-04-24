b7e56fb36055fc7ac859c1b8cb1f4bd547d6ed94
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-24 14:05:06 -0400

    Add gravatar to mailboxer messages sent by other users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def gravatar_for(user, size = 30, title = user.name)
+    image_tag user.gravatar_url(:email => user.email, size: size), title: title, class: "img-rounded"
+  end
 end
