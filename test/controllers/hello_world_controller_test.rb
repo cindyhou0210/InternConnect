7c20f17ba9526466f62e88602b17a5d0047883e6
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-28 18:30:08 -0400

    Add valid signup test, fix fixtures

--- test/controllers/hello_world_controller_test.rb
+++ test/controllers/hello_world_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class HelloWorldControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get hello_world_path
-    assert_response :success
-  end
 end
