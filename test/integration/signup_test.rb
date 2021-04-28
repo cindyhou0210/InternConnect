f5116762b1693ef9ce1a2a60cd628fafd1f72567
Dawn Mitchell <dmitchell@brandeis.edu>
2021-04-28 18:30:08 -0400

    Add valid signup test, fix fixtures

--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class SignupTest < ActionDispatch::IntegrationTest
-  
+
+  def setup
+    @review = reviews(:one)
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'UserConfid.count' do
@@ -11,5 +15,17 @@ class SignupTest < ActionDispatch::IntegrationTest
                                         password_confirmation: "pw" } }
     end
     assert_template 'user_confids/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'UserConfid.count', 1 do
+      post signup_path, params: { user_confid: { name: "Bob Example",
+                                        email: "bob@test.com",
+                                        password:              "password",
+                                        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'user_confids/show'
   end
 end
